2d84f309e947e171df52a7a6c585ce8d9b44a006
Hyftar <landry_simon@pm.me>
2023-11-11 14:40:56 -0500

    Simplify DateTimeExtensions

--- TaskTideAPI/DateExtensions/DateTimeExtensions.cs
+++ TaskTideAPI/DateExtensions/DateTimeExtensions.cs
@@ -8,72 +8,42 @@ namespace TaskTideAPI.DateExtensions
         {
             var startOfMonth = DateAdjusters.StartOfMonth(date);
 
-            if (startOfMonth.DayOfWeek == dayOfWeek)
-            {
-                return startOfMonth;
-            }
-
-            return startOfMonth.Next(dayOfWeek);
+            return DateAdjusters.NextOrSame(dayOfWeek)(startOfMonth);
         }
 
         public static LocalDate GetSecondDayOfMonth(this LocalDate date, IsoDayOfWeek dayOfWeek)
         {
             var startOfMonth = DateAdjusters.StartOfMonth(date);
 
-            if (startOfMonth.DayOfWeek == dayOfWeek)
-            {
-                return startOfMonth.PlusWeeks(1);
-            }
-
-            return startOfMonth.Next(dayOfWeek).PlusWeeks(1);
+            return DateAdjusters.NextOrSame(dayOfWeek)(startOfMonth).PlusWeeks(1);
         }
 
         public static LocalDate GetThirdDayOfMonth(this LocalDate date, IsoDayOfWeek dayOfWeek)
         {
             var startOfMonth = DateAdjusters.StartOfMonth(date);
 
-            if (startOfMonth.DayOfWeek == dayOfWeek)
-            {
-                return startOfMonth.PlusWeeks(2);
-            }
-
-            return startOfMonth.Next(dayOfWeek).PlusWeeks(2);
+            return DateAdjusters.NextOrSame(dayOfWeek)(startOfMonth).PlusWeeks(2);
         }
 
         public static LocalDate GetFourthDayOfMonth(this LocalDate date, IsoDayOfWeek dayOfWeek)
         {
             var startOfMonth = DateAdjusters.StartOfMonth(date);
 
-            if (startOfMonth.DayOfWeek == dayOfWeek)
-            {
-                return startOfMonth.PlusWeeks(3);
-            }
-
-            return startOfMonth.Next(dayOfWeek).PlusWeeks(3);
+            return DateAdjusters.NextOrSame(dayOfWeek)(startOfMonth).PlusWeeks(3);
         }
 
         public static LocalDate GetLastDayOfMonth(this LocalDate date, IsoDayOfWeek dayOfWeek)
         {
-            var startOfMonth = DateAdjusters.EndOfMonth(date);
+            var endOfMonth = DateAdjusters.EndOfMonth(date);
 
-            if (startOfMonth.DayOfWeek == dayOfWeek)
-            {
-                return startOfMonth;
-            }
-
-            return startOfMonth.Previous(dayOfWeek);
+            return DateAdjusters.PreviousOrSame(dayOfWeek)(endOfMonth);
         }
 
         public static LocalDate GetSecondLastDayOfMonth(this LocalDate date, IsoDayOfWeek dayOfWeek)
         {
-            var startOfMonth = DateAdjusters.EndOfMonth(date);
+            var endOfMonth = DateAdjusters.EndOfMonth(date);
 
-            if (startOfMonth.DayOfWeek == dayOfWeek)
-            {
-                return startOfMonth.Minus(Period.FromWeeks(1));
-            }
-
-            return startOfMonth.Previous(dayOfWeek).Minus(Period.FromWeeks(1));
+            return DateAdjusters.PreviousOrSame(dayOfWeek)(endOfMonth).PlusWeeks(-1);
         }
     }
 }
